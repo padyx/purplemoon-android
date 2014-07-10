9d3f6df10d67259a2385917b55b6a09a797762a0
Chakotay <BBpadyxsec2011>
2014-07-10 23:17:40 +0200

    drop toString options

--- purpleSky/src/main/java/ch/defiant/purplesky/beans/util/Pair.java
+++ purpleSky/src/main/java/ch/defiant/purplesky/beans/util/Pair.java
@@ -11,13 +11,7 @@ import java.io.Serializable;
 public class Pair<S extends Serializable, T extends Serializable> implements Serializable {
 
     private static final long serialVersionUID = -3289373175835990567L;
-    private TOSTRING_TYPE m_configToString;
 
-    public enum TOSTRING_TYPE {
-        DEFAULT,
-        FIRST,
-        SECOND
-    }
 
     private S m_first;
     private T m_second;
@@ -25,12 +19,6 @@ public class Pair<S extends Serializable, T extends Serializable> implements Ser
     public Pair(S first, T second) {
         setFirst(first);
         setSecond(second);
-    }
-
-    public Pair(S first, T second, TOSTRING_TYPE toStringType) {
-        setFirst(first);
-        setSecond(second);
-        setConfigToString(toStringType);
     }
 
     public void setFirst(S first) {
@@ -51,34 +39,13 @@ public class Pair<S extends Serializable, T extends Serializable> implements Ser
 
     @Override
     public String toString() {
-        TOSTRING_TYPE toStringOption = getConfigToString();
         StringBuilder sb = new StringBuilder();
-        switch (toStringOption) {
-            case DEFAULT:
-                sb.append("Pair[");
-                sb.append(getFirst());
-                sb.append(", ");
-                sb.append(getSecond());
-                sb.append("]");
-            case FIRST:
-                sb.append(getFirst());
-                break;
-            case SECOND:
-                sb.append(getSecond());
-                break;
-        }
+        sb.append("Pair[");
+        sb.append(getFirst());
+        sb.append(", ");
+        sb.append(getSecond());
+        sb.append("]");
         return sb.toString();
-    }
-
-    public TOSTRING_TYPE getConfigToString() {
-        return m_configToString;
-    }
-
-    public void setConfigToString(TOSTRING_TYPE configToString) {
-        if (configToString == null) {
-            configToString = TOSTRING_TYPE.DEFAULT;
-        }
-        m_configToString = configToString;
     }
 
 }
