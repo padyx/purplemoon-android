89af6ee37c9d945f1044194f66d84c4061014251
Patrick Bänziger <padyx@bluewin.ch>
2015-01-03 19:46:48 +0100

    Move and slightly refactor

--- purpleSky/src/main/java/ch/defiant/purplesky/fragments/event/EventFragment.java
+++ purpleSky/src/main/java/ch/defiant/purplesky/fragments/event/EventFragment.java
@@ -1,4 +1,4 @@
-package ch.defiant.purplesky.fragments;
+package ch.defiant.purplesky.fragments.event;
 
 import android.app.Activity;
 import android.app.AlertDialog;
@@ -6,7 +6,6 @@ import android.app.Dialog;
 import android.app.DialogFragment;
 import android.app.Fragment;
 import android.app.LoaderManager;
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.Loader;
@@ -22,8 +21,6 @@ import android.view.ViewGroup;
 import android.webkit.WebView;
 
 import java.io.Serializable;
-import java.util.Arrays;
-import java.util.Locale;
 
 import javax.inject.Inject;
 
@@ -31,12 +28,10 @@ import ch.defiant.purplesky.R;
 import ch.defiant.purplesky.api.promotions.EventRegistrationResult;
 import ch.defiant.purplesky.api.promotions.IPromotionAdapter;
 import ch.defiant.purplesky.beans.promotion.Event;
-import ch.defiant.purplesky.beans.promotion.EventLocation;
 import ch.defiant.purplesky.constants.ArgumentConstants;
-import ch.defiant.purplesky.core.PurpleSkyApplication;
+import ch.defiant.purplesky.fragments.BaseFragment;
 import ch.defiant.purplesky.loaders.promotions.EventLoader;
 import ch.defiant.purplesky.loaders.promotions.RegisterUnregisterLoader;
-import ch.defiant.purplesky.util.DateUtility;
 import ch.defiant.purplesky.util.Holder;
 
 /**
@@ -117,7 +112,7 @@ public class EventFragment extends BaseFragment implements LoaderManager.LoaderC
             m_registerMenu.setEnabled(false);
         } else {
             Event event = data.getContainedObject();
-            m_webview.loadData(promoToHtml(getActivity(), event), "text/html; charset=utf-8", "UTF-8");
+            m_webview.loadData(EventHTMLTranslator.promoToHtml(getActivity(), event), "text/html; charset=utf-8", "UTF-8");
 
             m_registerMenu.setEnabled(canRegister(event));
             m_registerMenu.setVisible(!event.isRegistered());
@@ -153,95 +148,6 @@ public class EventFragment extends BaseFragment implements LoaderManager.LoaderC
             return super.onOptionsItemSelected(item);
         }
     }
-
-    private static String promoToHtml(Context c, Event event) {
-        StringBuilder sb = new StringBuilder();
-        sb.append("<html><head><meta http-equiv='Content-Type' content='text/html; charset=utf-8'>");
-        sb.append("<style>td {padding: 8px} tr:nth-child(odd) {background-color:#BBE4FA;} " +
-                "tr:nth-child(even) {background-color:#AFD1E3;}</style>");
-        sb.append("</head>\n");
-        sb.append("<body><h2>");
-        sb.append(event.getEventName());
-        sb.append("</h2><p style='font-face: Verdana,Arial,sans-serif;'>");
-        sb.append(event.getDescriptionHtml());
-        sb.append("</p>\n");
-
-        if(event.isPreliminary()) {
-            sb.append("<p><em>");
-            sb.append(c.getString(R.string.PreliminaryEventNoRegistration));
-            sb.append("</em></p>");
-        }
-
-        sb.append("<p>");
-        String date = DateUtility.getMediumDateTimeString(event.getStart());
-        if(event.getEnd() != null){
-            date += " - ";
-            if (DateUtility.isSameDay(event.getStart(), event.getEnd())){
-                date += android.text.format.DateFormat.getTimeFormat(PurpleSkyApplication.get()).format(event.getEnd());
-            } else {
-                date += DateUtility.getMediumDateTimeString(event.getEnd());
-            }
-        }
-        sb.append(date);
-        sb.append("</p><h3>");
-
-        sb.append(c.getString(R.string.Admission));
-        sb.append("</h3><table style='border:0'>");
-        if(event.isPrivate()){
-            addRow(sb, c.getString(R.string.Admission), c.getString(R.string.Limited_PrivateEvent));
-        }
-        if(event.getGenders() != null){
-            Event.Genders genders = event.getGenders();
-            if(Arrays.asList(Event.Genders.MEN_ONLY, Event.Genders.WOMEN_ONLY).contains(genders)){
-                addRow(sb, c.getString(R.string.EntranceOnlyFor), c.getString(genders.resourceId));
-            }
-            else if (genders != Event.Genders.ALL) {
-                // We don't care about all
-                addRow(sb, c.getString(R.string.Audience), c.getString(genders.resourceId));
-            }
-        }
-        if(event.getMinAge() != null) {
-            addRow(sb, c.getString(R.string.MinimumAge), String.valueOf(event.getMinAge()));
-        }
-        if(event.getMaxAge() != null) {
-            addRow(sb, c.getString(R.string.MaximumAge), String.valueOf(event.getMaxAge()));
-        }
-        addRow(sb, c.getString(R.string.Registrations), String.valueOf(event.getRegistrations()));
-        sb.append("</table>");
-
-
-
-        if(event.getLocation() != null) {
-            EventLocation location = event.getLocation();
-            sb.append("<h3>");
-            sb.append(c.getString(R.string.EventLocation));
-            sb.append("</h3><table style='border:0'>");
-
-            addRow(sb, c.getString(R.string.Name), location.getLocationName());
-            addRow(sb, c.getString(R.string.Address), location.getAddress());
-            addRow(sb, c.getString(R.string.Country), new Locale("", location.getCountryCode().toUpperCase()).getDisplayCountry());
-
-            if(location.getWebsite() != null) {
-                addRow(sb,
-                        c.getString(R.string.Information),
-                        "<a href='"+location.getWebsite()+"'>"+c.getString(R.string.Website)+"</a>");
-            }
-
-            sb.append("</table>");
-        }
-        sb.append("</body></html>");
-
-        return sb.toString();
-    }
-
-    private static void addRow(StringBuilder sb, String firstCol, String secondCol) {
-        sb.append("<tr><td>");
-        sb.append(firstCol);
-        sb.append("</td><td>");
-        sb.append(secondCol);
-        sb.append("</td></tr>\n");
-    }
-
 
     public static class ChooserDialog extends DialogFragment {
 
